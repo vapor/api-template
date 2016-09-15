b3fa5d4a74cd8d2097429c01000aa2322e843d92
Tanner Nelson <me@tanner.xyz>
2016-09-15 18:02:32 -0400

    vapor 1.0

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "VaporApp",
+    name: "App",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 0)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0)
     ],
     exclude: [
         "Config",
