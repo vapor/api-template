eaa7f30feb7e0e3490dbe0281bcf68126c531912
Tanner Nelson <me@tanner.xyz>
2016-09-29 19:58:31 -0400

    update for 1.1

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporApp",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 0)
     ],
     exclude: [
         "Config",
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -3,9 +3,8 @@ import Vapor
 let drop = Droplet()
 
 drop.get { req in
-    let lang = req.headers["Accept-Language"]?.string ?? "en"
     return try drop.view.make("welcome", [
-    	"message": Node.string(drop.localization[lang, "welcome", "title"])
+    	"message": drop.localization[req.lang, "welcome", "title"]
     ])
 }
 
