8de174679e7280c3324b1bc1624120dd8809fe82
Tanner <me@tanner.xyz>
2017-05-19 17:57:27 +0100

    Update Config+Setup.swift

--- Sources/App/Config+Setup.swift
+++ Sources/App/Config+Setup.swift
@@ -18,9 +18,6 @@ extension Config {
     /// Add all models that should have their
     /// schemas prepared before the app boots
     private func setupPreparations() throws {
-        /// Note, there is a bug in the Swift compiler when trying to use += 
-        /// and an array of longer length than 1,
-        /// use append on the preparations array to work around this.
         preparations.append(Post.self)
     }
 }
