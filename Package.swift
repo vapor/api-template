5e71aaa45c9b55fdfe3ae2d4153df777e3a75dac
Tanner Nelson <me@tanner.xyz>
2017-12-19 12:13:28 -0500

    update to beta

--- Package.swift
+++ Package.swift
@@ -5,13 +5,10 @@ let package = Package(
     name: "VaporApp",
     dependencies: [
         // 💧 A server-side Swift web framework. 
-        .package(url: "https://github.com/vapor/vapor.git", .exact("3.0.0-alpha.x")),
+        .package(url: "https://github.com/vapor/vapor.git", .branch("beta")),
     ],
     targets: [
-        .target(
-            name: "App",
-            dependencies: ["Routing", "Service", "Vapor"]
-        ),
+        .target(name: "App", dependencies: ["Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
