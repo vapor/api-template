ac7db13360c196077359b9b69cb99a65f60c55b8
Tanner Nelson <me@tanner.xyz>
2018-01-11 17:13:22 -0500

    fix xcode fatal error issues

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,19 +1,27 @@
 import App
 import Service
 import Vapor
+import COperatingSystem // Remove this when Xcode bug is fixed
 
 var config = Config.default()
 var env = Environment.detect()
 var services = Services.default()
 
-try App.configure(&config, &env, &services)
+do {
+    try App.configure(&config, &env, &services)
 
-let app = try Application(
-    config: config,
-    environment: env,
-    services: services
-)
+    let app = try Application(
+        config: config,
+        environment: env,
+        services: services
+    )
 
-try App.boot(app)
+    try App.boot(app)
 
-try app.run()
+    try app.run()
+} catch {
+    // Xcode 9.1 crashes sometimes when top level errors are thrown.
+    // This can be removed when that bug is fixed.
+    print(error)
+    exit(1)
+}
