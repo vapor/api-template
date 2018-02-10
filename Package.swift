baab34a987ad14a215e0f2bc0feb174cabefac53
tanner0101 <me@tanner.xyz>
2018-02-09 22:26:37 -0500

    beta 1

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ let package = Package(
     name: "VaporApp",
     dependencies: [
         // 💧 A server-side Swift web framework. 
-        .package(url: "https://github.com/vapor/vapor.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/vapor.git", .exact("3.0.0-beta.1")),
 
         // 🖋 Swift ORM framework (queries, models, and relations) for building NoSQL and SQL database integrations.
-        .package(url: "https://github.com/vapor/fluent.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/fluent.git", .exact("3.0.0-beta.1")),
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
