d54e9b5a23c8fc3c3875971bfe5b4ea09fc9e4db
Alex Reilly <alexander.r.reilly@gmail.com>
2018-02-11 10:22:15 -0800

    Wraps main in a do/catch

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -2,18 +2,23 @@ import App
 import Service
 import Vapor
 
-var config = Config.default()
-var env = try Environment.detect()
-var services = Services.default()
-
-try App.configure(&config, &env, &services)
-
-let app = try Application(
-    config: config,
-    environment: env,
-    services: services
-)
-
-try App.boot(app)
-
-try app.run()
+// The contents of main are wrapped in a do/catch block because any errors that get raised to the top level will crash XCode
+do {
+    var config = Config.default()
+    var env = try Environment.detect()
+    var services = Services.default()
+    
+    try App.configure(&config, &env, &services)
+    
+    let app = try Application(
+        config: config,
+        environment: env,
+        services: services
+    )
+    
+    try App.boot(app)
+    
+    try app.run()
+} catch {
+    print(error)
+}
