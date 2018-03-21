50931a7a271b41e44d0a56fa367997baac63e2b2
Tanner <me@tanner.xyz>
2018-03-21 17:46:33 -0400

    Update boot.swift

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -6,11 +6,4 @@ import Vapor
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#bootswift)
 public func boot(_ app: Application) throws {
     // your code here
-
-    let conn = try app.requestConnection(to: .sqlite).wait()
-    defer { app.releaseConnection(conn, to: .sqlite) }
-
-    for _ in 0..<1_000 {
-        _ = try Todo(title: "Hello World").save(on: conn).wait()
-    }
 }
