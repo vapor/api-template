6c4c4742e1b4a0321b1864d1f9920a1c295a168c
tanner0101 <me@tanner.xyz>
2018-03-25 01:13:10 -0400

    updates

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,28 +3,28 @@ import Vapor
 
 /// Called before your application initializes.
 ///
-/// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#configureswift)
+/// https://docs.vapor.codes/3.0/getting-started/structure/#configureswift
 public func configure(
     _ config: inout Config,
     _ env: inout Environment,
     _ services: inout Services
 ) throws {
-    // Register providers first
+    /// Register providers first
     try services.register(FluentSQLiteProvider())
 
-    // Register routes to the router
+    /// Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
 
-    // Register middleware
+    /// Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-    // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+    /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(DateMiddleware.self) // Adds `Date` header to responses
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
-
+    // Configure a SQLite database
     let sqlite: SQLiteDatabase
     if env.isRelease {
         /// Create file-based SQLite db using $SQLITE_PATH from process env
@@ -33,15 +33,13 @@ public func configure(
         /// Create an in-memory SQLite database
         sqlite = try SQLiteDatabase(storage: .memory)
     }
-    services.register(sqlite)
 
-
-    // Configure a SQLite database
+    /// Register the configured SQLite database to the database config.
     var databases = DatabaseConfig()
-    try databases.add(database: SQLiteDatabase(storage: .memory), as: .sqlite)
+    databases.add(database: sqlite, as: .sqlite)
     services.register(databases)
 
-    // Configure migrations
+    /// Configure migrations
     var migrations = MigrationConfig()
     migrations.add(model: Todo.self, database: .sqlite)
     services.register(migrations)
