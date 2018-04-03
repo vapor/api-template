7630db9548e14d227ba6ce5a18eaadf955b59df5
tanner0101 <me@tanner.xyz>
2018-04-02 21:25:25 -0400

    bcrypt updates

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -10,18 +10,6 @@ public func routes(_ router: Router) throws {
         return "Hello, world!"
     }
 
-    // Example of creating a Service and using it.
-    router.get("hash", String.parameter) { req -> String in
-        // Create a BCryptHasher using the Request's Container
-        let hasher = try req.make(BCryptHasher.self)
-
-        // Fetch the String parameter (as described in the route)
-        let string = try req.parameter(String.self)
-
-        // Return the hashed string!
-        return try hasher.make(string)
-    }
-
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
