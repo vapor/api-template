7b04d8a3c659225cf938bdcab2f9e148a6f6e508
tanner0101 <me@tanner.xyz>
2018-07-19 14:07:21 -0400

    update to release FluentSQLite deps

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2")
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
