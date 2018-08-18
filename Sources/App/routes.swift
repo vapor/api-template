546cc9c0bceac89c0edbfea4e58871800aafdbc7
Jonas Schwartz <jonas@qutheory.io>
2018-08-18 13:54:09 +0200

    Adding it works page

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,6 +2,12 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
+    
+    // Basic "It works" example
+    router.get { req in
+        return "It works!"
+    }
+    
     // Basic "Hello, world!" example
     router.get("hello") { req in
         return "Hello, world!"
