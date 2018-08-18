9ce535d55b0159185a18b5d6b8062a26281116f0
Jonas Schwartz <jonas@qutheory.io>
2018-08-18 13:55:32 +0200

    Removing extra whitespace

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,7 +2,6 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    
     // Basic "It works" example
     router.get { req in
         return "It works!"
