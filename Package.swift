8fccf26e07a7d5d211037aab709c1e4ca8e87aea
Ben Syverson <ben@bensyverson.com>
2019-03-06 18:54:43 -0600

    Allow the Vapor app to be used as a library

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "VaporApp",
+    products: [
+        .library(name: "VaporApp", targets: ["App"]),
+    ],
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
