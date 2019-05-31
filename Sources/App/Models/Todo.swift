ebef66b2ede1bb0ba04cb96fa1e0e24240fbe330
Tanner Nelson <me@tanner.xyz>
2019-05-31 17:21:47 -0400

    fluent updates

--- Sources/App/Models/Todo.swift
+++ Sources/App/Models/Todo.swift
@@ -3,11 +3,11 @@ import Vapor
 
 /// A single entry of a Todo list.
 struct Todo: Model {
-    static let `default` = Todo()
+    static let shared = Todo()
     static let entity = "todos"
     
     /// The unique identifier for this `Todo`.
-    let id = Field<Int>("id")
+    let id = Field<Int?>("id")
     /// A title describing what this `Todo` entails.
     let title = Field<String>("title")
     
