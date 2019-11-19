9c9a4e56654a3e4f5a70338086e4ed34e0009355
tanner0101 <me@tanner.xyz>
2019-11-19 16:46:23 -0500

    service updates

--- Sources/App/app.swift
+++ Sources/App/app.swift
@@ -3,7 +3,7 @@ import Vapor
 public func app(_ environment: Environment) throws -> Application {
     var environment = environment
     try LoggingSystem.bootstrap(from: &environment)
-    let app = Application(environment: environment)
+    let app = Application(environment)
     try! configure(app)
     try app.boot()
     try! boot(app)
