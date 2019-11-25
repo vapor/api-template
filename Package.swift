0f86df22e098e5f2c3e4d7875cfa195fbc4c61f5
Jimmy Arts <arts.jimmy@gmail.com>
2019-11-25 04:23:25 +0100

    Change MacOS platform version to v10_14 (#84)

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "app",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v10_14)
     ],
     products: [
         .executable(name: "Run", targets: ["Run"]),
